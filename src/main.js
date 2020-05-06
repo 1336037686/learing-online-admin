71930658d7e01bbb8c6202b7f01d15b5a23b6a23
LGX <1336037686@qq.com>
2020-05-06 23:31:43 +0800

    修改登陆bug

--- src/main.js
+++ src/main.js
@@ -17,32 +17,37 @@ let whiteList = ["/login", "/"]
 // 默认地址
 let defaultTo = "/admin/welcome"
 
-// //路由全局守卫, 登录权限判断
-// router.beforeEach((to, from, next)=> {
-//     // 白名单放行
-//     whiteList.forEach(function (e) {
-//       if(to.path == e) {
-//         next()
-//       }
-//     })
-//
-//     //权限验证
-//     let userName = store.state.loginManage.userName
-//     let token = store.state.loginManage.token
-//
-//     if(userName.trim() != "" && token.trim() != ""){
-//       // 如果用户已登陆,且跳转的是登陆界面,则直接定位到默认地址
-//       if(to.path == "/" || to.path == "/login") {
-//         next(defaultTo)
-//       } else {
-//         next()
-//       }
-//     } else {
-//       Notice.error({title: "请先登陆" , desc: ''});
-//       next("/")
-//     }
-//   }
-// );
+//路由全局守卫, 登录权限判断
+router.beforeEach((to, from, next)=> {
+    // 白名单放行
+    whiteList.forEach(function (e) {
+      if(to.path == e) {
+        next()
+      }
+    })
+
+    //权限验证
+    let userInfo = sessionStorage.getItem("userInfo")
+    if(userInfo == null) {
+      next("/")
+    } else {
+      userInfo = JSON.parse(sessionStorage.getItem("userInfo"))
+      let userName = userInfo.userName
+      let token = userInfo.token
+      if(userName.trim() != "" && token.trim() != ""){
+        // 如果用户已登陆,且跳转的是登陆界面,则直接定位到默认地址
+        if(to.path == "/" || to.path == "/login") {
+          next(defaultTo)
+        } else {
+          next()
+        }
+      } else {
+        // Notice.error({title: "请先登陆" , desc: ''});
+        next("/")
+      }
+    }
+  }
+);
 
 
 
